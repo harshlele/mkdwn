e2d5088ba35346574cc3d8bfc0e89059efb38e1b
Harshal Lele <harshallele97@gmail.com>
2020-09-16 23:45:33 +0530

    completed TopBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React from 'react'
 import TopBar from './components/TopBar'
+import PanelSelector from './components/PanelSelector'
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <TopBar/>
+      <PanelSelector/>
     </div>
   );
 }
--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,9 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react'
 import './index.scss'
+import CloudIcon from './assets/cloud-36dp.svg'
+import DownloadIcon from './assets/dw-36dp.svg'
 
 export default function TopBar() {
 
   const title:string = "Markdown Editor";
+
+  const onCloudClick = function (event: Object) {
+    const e = {...event};
+    console.log(e);
+  }
+
+  const onDwClick = function (event: Object) {
+    const e = {...event};
+    console.log(e);
+  }
 
   return (
     <div className="heading">
@@ -11,7 +23,8 @@ export default function TopBar() {
         <span>{title}</span>
       </div>
       <div className="btn-bar">
-        buttons
+        <img src={CloudIcon} title="Save to Google Drive" alt="Save to Google Drive" onClick={onCloudClick}/>
+        <img src={DownloadIcon} title="Download" alt="Download" onClick={onDwClick}/>
       </div>
     </div>
   )
